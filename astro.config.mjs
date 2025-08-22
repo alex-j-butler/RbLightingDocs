017ab2ac4e27460f77b8d28a36dc0bfc5b0ce9e9
Alex Butler <a.jbutler019@gmail.com>
2025-08-22 20:48:45 +1000

    More changes...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: cloudflare(),
 	integrations: [
 		starlight({
 			title: 'RbLighting Docs',
