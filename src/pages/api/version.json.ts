091f531b041c64cad7f48b64f482651116a8451e
Alex Butler <a.jbutler019@gmail.com>
2025-11-10 13:22:02 +1000

    Version 1.2.5 release.

--- src/pages/api/version.json.ts
+++ src/pages/api/version.json.ts
@@ -1,7 +1,7 @@
 export function GET({ }) {
   return new Response(
     JSON.stringify({
-        version: "1.2.4.43565",
+        version: "1.2.5.42533",
         download_url: "https://rblighting.albiedj.com/downloads/RbLighting_1.2.4.msi"
     }),
   );
