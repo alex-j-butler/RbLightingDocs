9602958d1e9ceffca6889287ff8fe0e945820080
Alex Butler <a.jbutler019@gmail.com>
2025-11-13 18:43:05 +1000

    Version 1.2.6 release.

--- src/pages/api/version.json.ts
+++ src/pages/api/version.json.ts
@@ -1,8 +1,9 @@
 export function GET({ }) {
   return new Response(
     JSON.stringify({
-        version: "1.2.5.42533",
-        download_url: "https://rblighting.albiedj.com/downloads/RbLighting_1.2.5.msi"
+        version: "1.2.6.41985",
+        download_url: "https://rblighting.albiedj.com/downloads/RbLighting_1.2.6.msi",
+        changelog: "Added support for Rekordbox 7.2.7"
     }),
   );
 }
