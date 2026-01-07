8e51d034e919d54cb5643b339cdc1668da354be0
Alex Butler <a.jbutler019@gmail.com>
2026-01-07 13:15:49 +1000

    Version 1.3.26 release.

--- src/pages/api/version.json.ts
+++ src/pages/api/version.json.ts
@@ -1,9 +1,9 @@
 export function GET({ }) {
   return new Response(
     JSON.stringify({
-        version: "1.2.7.56306",
-        download_url: "https://rblighting.albiedj.com/downloads/RbLighting_1.2.7.msi",
-        changelog: "Added support for Rekordbox 7.2.8"
-    }),
+      version: "1.3.26.39183",
+      download_url: "https://rblighting.albiedj.com/downloads/RbLighting_1.3.26.msi",
+      changelog: "UI Overhaul\nAdded support for Rekordbox 7.2.4\nAdded support for Rekordbox 7.2.7\n\nAdded support for Rekordbox 6.8.6\nBackport: Fixed SoundSwitch integration sending wrong values for Deck 2 time."
+  }),
   );
 }
